b7e8c0372ccdf833fd63d42611b2498650cc17a4
himayakaumadi <en102870@foe.sjp.ac.lk>
2024-01-10 11:50:23 +0530

    calculate_cell_value() function

--- Grid/calculate_cell_value.py
+++ Grid/calculate_cell_value.py
@@ -8,4 +8,4 @@ def calculate_cell_value(self):
                     for j in range(max(0, y - 1), min(self.size, y + 2)):
                         if self.grid[i][j][0] == -1:
                             count += 1
-                self.grid[x][y] = (count, self.grid[x][y][1])
+                self.grid[x][y] = [count, self.grid[x][y][1]]
