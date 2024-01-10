c4a62118124d22c19b6fcc31f11d492cdb6ddfed
Namindu <namindu@live.com>
2024-01-10 11:51:27 +0530

    reveal funct

--- Grid/__init__.py
+++ Grid/__init__.py
@@ -3,7 +3,7 @@ class Grid:
         self.size = size
         self.num_mines = num_mines
         self.revealed_cells = 0
-        self.max_cells = 0
+        self.max_cells = (size * size) - num_mines
          # [<num_mines>, <status>] - 0: not revealed, 1: revealed, 2: flagged
         self.grid = [[[0,0]for _ in range(size)]for _ in range(size)]
 
@@ -15,7 +15,7 @@ class Grid:
     # for Namindu
     from .reveal import reveal
     from .cell_status_const import cell_status
-
+    from .reveal_cells_recursive import reveal_cells_recursive
     ##############################
 
     from .place_mines import place_mines
--- test.py
+++ test.py
@@ -6,9 +6,12 @@ import Grid
 import random
 
 cg = Grid.Grid(4, 4)
-cg.grid[1][1] = [-1,1]
-cg.grid[1][2] = [1,1]
-cg.grid[2][2] = [1,2]
+cg.grid[1][1] = [-1,0]
+cg.grid[1][2] = [1,0]
+cg.grid[2][2] = [1,0]
+cg.print_grid()
+
+print("Revealing...")
 cg.reveal(0,0)
 cg.print_grid()
 
