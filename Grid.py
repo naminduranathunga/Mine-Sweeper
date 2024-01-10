9c7a48759568278c01e5ce730a4dd33ba4840856
Namindu <namindu@live.com>
2024-01-10 09:37:49 +0530

    boilerplate

---
+++ Grid.py
@@ -0,0 +1,22 @@
+"""
+comments
+"""
+class Grid:
+    def __init(self):
+        pass
+
+    def place_mines(self):
+        pass
+
+    def place_numbers(self):
+        pass
+
+    def print_grid(self):
+        pass
+    
+    def reveal(self, x, y):
+        pass
+
+    def place_flag(self, x, y):
+        pass
+
